1e621368392e527d6429c8800857ff3c61cece9c
Adam <adamkdevelop@gmail.com>
2020-09-20 13:36:25 -0700

    Added base Prototype

---
+++ Assets/Scripts/GrabHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHandler : MonoBehaviour
+{
+    public float grabAnimDistance = 3.0f;
+    private void Start()
+    {
+        Physics.IgnoreCollision(GetComponent<Collider>(), transform.parent.GetComponent<Collider>());
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        PlayerHandler targetPH = collider.gameObject.transform.Find("BaseModel").GetComponent<PlayerHandler>();
+        PlayerHandler selfPH = transform.parent.Find("BaseModel").GetComponent<PlayerHandler>();
+        if (!targetPH.canBeGrabbed())
+            return;
+
+        Vector3 grabLoc = Vector3.MoveTowards(targetPH.transform.parent.position, transform.position, 100.0f);
+        targetPH.transform.parent.position = new Vector3(grabLoc.x, targetPH.transform.parent.position.y, grabLoc.z);
+        targetPH.rotTowards(selfPH.gameObject.transform.position);
+        targetPH.grabMe();
+        selfPH.grabConnected();
+    }
+}
---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    private Animator anim;
+    private float rotSpeed = 100.0f;
+    public float speed = 1.5f;
+    public bool controllable = false;
+
+    void Start()
+    {
+
+        
+        speed = 1.5f;
+        AnimationClip animClip = null;
+        anim = gameObject.GetComponent<Animator>();
+
+        AnimationEvent animEventGrabbing = new AnimationEvent();
+        animEventGrabbing.intParameter = 1;
+        animEventGrabbing.time = 0.0f;
+        animEventGrabbing.functionName = "setGrab";
+
+        AnimationEvent animEventGrabEnd = new AnimationEvent();
+        animEventGrabEnd.intParameter = 0;
+        animEventGrabEnd.time = 0.165f;
+        animEventGrabEnd.functionName = "setGrab";
+
+        foreach (AnimationClip tempClip in anim.runtimeAnimatorController.animationClips)
+        {
+            if (tempClip.name == "Armature|GrabActive")
+            {
+                animClip = tempClip;
+            }
+        }
+
+        if (animClip != null)
+        {
+            animClip.AddEvent(animEventGrabbing);
+            animClip.AddEvent(animEventGrabEnd);
+        }
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controllable)
+        {
+            if (Input.GetKeyDown("z"))
+            {
+                if ((anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|Walk") || anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|Idle")))
+                anim.SetTrigger("GrabStartParam");
+            }
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|Walk") || anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|Idle"))
+            {
+                Vector3 targetVec = new Vector3();
+                if (Input.GetKey("up"))
+                {
+                    anim.SetFloat("WalkParam", speed);
+                    targetVec += new Vector3(0, 0, 1);
+                }
+                if (Input.GetKey("down"))
+                {
+                    anim.SetFloat("WalkParam", speed);
+                    targetVec += new Vector3(0, 0, -1);
+                }
+                if (Input.GetKey("left"))
+                {
+                    anim.SetFloat("WalkParam", speed);
+                    targetVec += new Vector3(-1, 0, 0);
+                }
+                if (Input.GetKey("right"))
+                {
+                    anim.SetFloat("WalkParam", speed);
+                    targetVec += new Vector3(1, 0, 0);
+                }
+                if (targetVec == new Vector3())
+                {
+                    anim.SetFloat("WalkParam", 0.0f);
+                }
+                if (targetVec != new Vector3())
+                {
+                    targetVec = targetVec.normalized;
+                    rotTowards(targetVec + transform.parent.position);
+                    transform.parent.Translate(targetVec.x * speed * Time.deltaTime, targetVec.y * speed * Time.deltaTime, targetVec.z * speed * Time.deltaTime, Space.World);
+                }
+            }
+        }
+    }
+
+    public void setGrab(int value)
+    {
+        Debug.Log("Setting grab: " + value);
+        transform.parent.Find("GrabHitbox").gameObject.GetComponent<BoxCollider>().enabled = value == 1;
+    }
+
+    public void grabConnected()
+    {
+        anim.SetTrigger("GrabConnectParam");
+    }
+
+    public bool canBeGrabbed()
+    {
+        return !(anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|Grabbed")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|GetUp")
+            || anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|GrabConnect"));
+    }
+
+    public void grabMe()
+    {
+        anim.SetTrigger("GrabbedParam");
+    }
+
+    public void rotTowards(Vector3 target)
+    {
+        Vector3 targetDirection = target - transform.parent.position;
+        float singleStep = rotSpeed * Time.deltaTime;
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.parent.forward, targetDirection, singleStep, 0.0f);
+        transform.parent.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
