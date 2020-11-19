21a7e7267174473d6d726dea7e05f3aa88d9dbe0
Erik Ho <eho61@my.bcit.ca>
2020-11-18 20:07:15 -0800

    Connected Titlescreen

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -5,19 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void LoadScene(string scene) {
         SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
     }
 }
