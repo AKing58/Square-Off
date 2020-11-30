2d1fa76055171b4f7b72259f51b3194c3b7b13b5
JustinKwok13 <jkwok51@my.bcit.ca>
2020-11-29 19:38:22 -0800

    FireStage
    Added rotating fire trap on the round stage. Deals damage and staggers players who touch it.

---
+++ Assets/Scripts/FireDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDamage : MonoBehaviour
+{
+    public float fireDamage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<PlayerHandler>().CanBeStriked())
+        {
+            other.gameObject.GetComponent<PlayerHandler>().Health -= fireDamage;
+            other.gameObject.GetComponent<PlayerHandler>().StrikeMe();
+
+            if (other.gameObject.GetComponent<PlayerHandler>().Health <= 0)
+            {
+                other.gameObject.GetComponent<PlayerHandler>().StageKillMe();
+            }
+        }
+    }
+}
